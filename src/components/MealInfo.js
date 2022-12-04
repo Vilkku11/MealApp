892d0165440564f082d0cd3d4477a42bf366b083
William Reima <william.reima@gmail.com>
2022-12-05 00:15:41 +0200

    extracting ingredients and measures working

--- src/components/MealInfo.js
+++ src/components/MealInfo.js
@@ -1,12 +1,15 @@
 import { getValue } from "@testing-library/user-event/dist/utils";
 import { useState, useEffect } from "react";
 
+import "./MealInfo.css";
+
 const MealInfo = (props) => {
   const [img, setImg] = useState([]);
 
   const data = props.data;
-  let ingredients = [];
-  let measures = [];
+  //let ingredients = [];
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
 
   console.log(data.strMealThumb);
 
@@ -20,26 +23,48 @@ const MealInfo = (props) => {
     setImg(imageObjectURL);
   };
 
+  // Extract ingredients and measurements
   const getIngredients = () => {
-    let ingredient = "strIngredient1";
-    console.log(data[ingredient]);
-    for (var key in data) {
-      if (key.includes("strIngredient") == true) {
-        ingredients.push(key);
-        //console.log(valueOf(key));
+    let ingredients = [];
+    let measures = [];
+
+    for (let i = 1; i < 21; i++) {
+      let ingredient = "strIngredient" + i;
+      let measure = "strMeasure" + i;
+      if (data[ingredient] && data[ingredient] !== " ") {
+        ingredients.push(data[ingredient]);
+      }
+      if (data[measure] && data[measure] !== " ") {
+        measures.push(data[measure]);
       }
     }
-    console.log(ingredients);
+    setIngredients(ingredients);
+    setMeasures(measures);
+  };
+
+  // Renders given array
+  const renderArray = (array) => {
+    console.log(array);
+    return (
+      <ul>
+        {array.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    );
   };
 
   useEffect(() => {
     fetchImage();
     getIngredients();
   }, [props]);
+
   return (
-    <div>
+    <div className="mealInfo">
       <h1>{data.strMeal}</h1>
       <img src={img} alt="Could not load"></img>
+      <div>{renderArray(ingredients)}</div>
+      <div>{renderArray(measures)}</div>
     </div>
   );
 };
