aa1037fd94107fddf3337506d49206883d9bec78
William Reima <william.reima@gmail.com>
2022-12-08 17:58:24 +0200

    added firebase realtime database, button now sending to it in mealinfopage

--- src/pages/MealInfoPage.js
+++ src/pages/MealInfoPage.js
@@ -1,14 +1,16 @@
 import { useHistory } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
 import MealInfo from "../components/MealInfo";
 import "./MealInfoPage.css";
+import { useAuth } from "../contexts/AuthContext";
 
 const MealInfoPage = (props) => {
   const history = useHistory();
 
   const [data, setData] = useState([]);
-  //let data = "";
+  const { currentUser } = useAuth();
 
   const backToMain = () => {
     history.push("/");
@@ -21,12 +23,33 @@ const MealInfoPage = (props) => {
     });
   };
 
+  const addToList = async () => {
+    const url =
+      process.env.REACT_APP_FIREBASE_DATABASE +
+      currentUser.email.replace(".", "_") +
+      ".json";
+    console.log(url);
+
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const answer = await response.json();
+    console.log(answer);
+  };
+
   useEffect(() => {
     getMeal();
   }, []);
   return (
     <div>
-      <button onClick={backToMain}>back to main</button>
+      <div>
+        {currentUser ? <Button onClick={addToList}>Add to list</Button> : ""}
+      </div>
+      <Button onClick={backToMain}>back to main</Button>
       <div className="mealInfo">
         <MealInfo data={data} img={props.img}></MealInfo>
       </div>
